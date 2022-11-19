d986ec48b991bcfe32d7dec218cde4b3c971bb32
samubura <samuele.bura98@gmail.com>
2022-11-19 19:35:59 +0100

    Let's bounce out of here

--- Assets/MoveBall.cs
+++ Assets/MoveBall.cs
@@ -14,13 +14,7 @@ public class MoveBall : MonoBehaviour
     {
         Direction = new Vector3(0, 0, 0);
         Speed = 0;
-        this.gameObject.GetComponent<WallDetection>().WallHitEvent.AddListener(() => WallHit());
         _rigidbody = gameObject.GetComponent<Rigidbody>();
-    }
-
-    private void WallHit()
-    {
-        this.Direction = this.Direction * -1;
     }
 
     // Update is called once per frame
