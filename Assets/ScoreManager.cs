6a1b1fd73eda4593e4fe234c430361dd8e2d5d3c
samubura <samuele.bura98@gmail.com>
2022-11-21 18:09:22 +0100

    It adds up

--- Assets/ScoreManager.cs
+++ Assets/ScoreManager.cs
@@ -1,31 +1,40 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class ScoreManager : MonoBehaviour
 {
+    [SerializeField]
+    private TextMeshProUGUI textField;
     private int score = 0;
     // Start is called before the first frame update
     void Start()
     {
         this.gameObject.GetComponent<DetectTrigger>().CollisionEvent.AddListener(x => PickupHit(x));
         this.gameObject.GetComponent<WallDetection>().WallHitEvent.AddListener(() => WallHit());
+        ShowScore();
+    }
+
+    private void ShowScore()
+    {
+        textField.text = $"Score: {score}";
     }
 
     private void WallHit()
     {
         score = score - 1;
-        Debug.Log("Score: " + score);
+        ShowScore();
     }
 
     private void PickupHit(Collider collider)
     {
-       if(collider.gameObject.tag == "Pickup")
+        if (collider.gameObject.tag == "Pickup")
         {
             score = score + 1;
         }
-        Debug.Log("Score: " + score);
+        ShowScore();
     }
 
     // Update is called once per frame
